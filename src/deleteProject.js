3fec9598ab9e681a426fc85037b9c61e3571dd9f
Jaakul <jackw1689@gmail.com>
2022-10-28 16:59:36 -0700

    eslint configure 'no-console':off

--- src/deleteProject.js
+++ src/deleteProject.js
@@ -3,9 +3,9 @@ import { getTodoList } from './getTodoList';
 import populateProjects from './populateProjects';
 
 export default function deleteProject() {
-  if (confirm('Are you sure you want to delete this project and all of its contents')) {
+  if (confirm('Are you sure you want to delete this project and all of its contents?')) {
     const list = JSON.parse(localStorage.getItem('list'));
-    for (let i = list.length - 1; i >= 0; i--) {
+    for (let i = list.length - 1; i >= 0; i -= 1) {
       if (window.headerPointer === list[i].project) {
         list.splice(i, 1);
       }
